4dee43679d9c4fe48d1263759c0cd33b1ba5d541
Pratik <pratik211092@gmail.com>
2015-03-09 19:14:18 +0530

    added whitenoise

--- aiet/settings.py
+++ aiet/settings.py
@@ -106,6 +106,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT =  os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aiet.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
